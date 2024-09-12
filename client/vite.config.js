2945f71cea01d4d01e399bb0283b16f34e60cfe1
uzaaannn <108566614+evandaru@users.noreply.github.com>
2024-09-12 22:27:02 +0700

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Ensure this matches the directory Vercel is looking for
+    outDir: 'build',
   },
 })
